81876ae9255443be2c123f1f4012d64a366efbad
shaoyonghua <shaoyonghua@csii.com.cn>
2019-07-15 17:23:37 +0800

    feat: add some pluginOptions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = (api, options) => {
   api.chainWebpack(webpackConfig => {
+    const pluginOptions = options.pluginOptions || {};
+
     webpackConfig
       .plugin('storee')
-      .use(require('./lib/StoreePlugin'))
+      .use(require('./lib/StoreePlugin'), [{
+        ...(pluginOptions.storee || {})
+      }])
   })
 };
